9660074ee4548b754cfa86607adb28902c6c1ff0
angelicapriscila <apss11@aluno.ifal.edu.br>
2024-08-05 20:32:31 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+//import 'package:lib/Ice_Cream.dart';
+
+
+import 'Ice_Cream.dart';
+
+
+
+
+
+
+
+
+void main() {
+ runApp(
+   const MaterialApp(
+     home: Ice_Cream(),
+   ),
+ );
+}
